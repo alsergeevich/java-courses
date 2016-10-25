40e3cb8b1a17558bf720640faaabe8a9004bb170
alsergeevich <alsergeevich@gmail.com>
2016-10-25 21:31:52 +0500

    Добавил класс с действиями и изменил калькулятор.

---
+++ Calc.java
@@ -0,0 +1,26 @@
+public class Calc {
+	
+	double rez;
+	
+	public double plus (double a, double b){
+		
+		return rez = a + b;
+	}
+	
+	public double minus (double a, double b){
+		
+		return rez = a - b;
+	}
+	
+	public double umnoj (double a, double b){
+		
+		return rez = a * b;
+	}
+	
+	public double delenie (double a, double b){
+		
+		return rez = a / b;
+	}
+	
+	
+}
--- Calculate.java
+++ Calculate.java
@@ -1,7 +1,42 @@
+import java.util.Scanner;
 public class Calculate {
 	
 	public static void main (String[] args) {
 		
-		System.out.println("Calculate");
+	try{	
+		String exit = "no";
+		Scanner scanner = new Scanner(System.in);
+		Calc calc = new Calc();
+		double rez = 0;
+		int deysvie;
+		double a;
+		double b;
+		while( !exit.equals("yes")){
+			System.out.println("Input first number:");
+			a = scanner.nextDouble();
+			System.out.println("Input second number:");
+			b = scanner.nextDouble();
+			System.out.println("Select app: ");
+			System.out.println();
+			System.out.println("1. Summ");
+			System.out.println("2. Minus");
+			System.out.println("3. Umnojenie");
+			System.out.println("4. Delenie");
+			deysvie = scanner.nextInt();
+			if(deysvie == 1) rez = calc.plus(a, b);
+			if(deysvie == 2) rez = calc.minus(a,b);
+			if(deysvie == 3) rez = calc.umnoj(a, b);
+			if(deysvie == 4) rez = calc.delenie(a, b);
+			System.out.println("Rezultat = " + rez);
+			System.out.println("Exit yes/no");
+			exit = scanner.next();
+		}
+	}finally {
+		return;
+	}
+		
+		
+		
+		
 	}
 }
